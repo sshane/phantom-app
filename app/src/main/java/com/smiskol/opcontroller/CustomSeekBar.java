a3bb7df5dc7119a5d0d8b3152c5bf0a4275357c9
Shane <shane@smiskol.com>
2019-03-15 17:41:16 -0500

    Some app design work

---
+++ app/src/main/java/com/smiskol/opcontroller/CustomSeekBar.java
@@ -0,0 +1,77 @@
+package com.smiskol.opcontroller;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatSeekBar;
+import android.util.AttributeSet;
+import android.widget.SeekBar;
+
+public class CustomSeekBar extends AppCompatSeekBar {
+
+    private Rect rect;
+    private Paint paint ;
+    private int seekbar_height;
+
+    public CustomSeekBar(Context context) {
+        super(context);
+
+    }
+
+    public CustomSeekBar(Context context, AttributeSet attrs) {
+
+        super(context, attrs);
+        rect = new Rect();
+        paint = new Paint();
+        seekbar_height = 6;
+    }
+
+    public CustomSeekBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected synchronized void onDraw(Canvas canvas) {
+
+        rect.set(0 + getThumbOffset()+5,
+                (getHeight() / 2) - (seekbar_height/2)-2,
+                getWidth()- getThumbOffset()-5,
+                (getHeight() / 2) + (seekbar_height/2));
+
+        paint.setColor(Color.parseColor("#CCCCCC"));
+
+        canvas.drawRect(rect, paint);
+
+
+
+        if (this.getProgress() > 50) {
+
+
+            rect.set(getWidth() / 2,
+                    (getHeight() / 2) - (seekbar_height/2),
+                    getWidth() / 2 + (getWidth() / 100) * (getProgress() - 50),
+                    getHeight() / 2 + (seekbar_height/2));
+
+            paint.setColor(ContextCompat.getColor(getContext(), R.color.colorAccent));
+            canvas.drawRect(rect, paint);
+
+        }
+
+        if (this.getProgress() < 50) {
+
+            rect.set(getWidth() / 2 - ((getWidth() / 100) * (50 - getProgress())),
+                    (getHeight() / 2) - (seekbar_height/2),
+                    getWidth() / 2,
+                    getHeight() / 2 + (seekbar_height/2));
+
+            paint.setColor(ContextCompat.getColor(getContext(), R.color.colorAccent));
+            canvas.drawRect(rect, paint);
+
+        }
+
+        super.onDraw(canvas);
+    }
+}
--- app/src/main/java/com/smiskol/opcontroller/MainActivity.java
+++ app/src/main/java/com/smiskol/opcontroller/MainActivity.java
@@ -2,12 +2,57 @@ package com.smiskol.opcontroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    CustomSeekBar steerSeekBar;
+    TextView steerTextView;
+    TextView accelTextView;
+    SeekBar accelSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        steerSeekBar = findViewById(R.id.steerSeekBar);
+        steerTextView = findViewById(R.id.steerTextView);
+        accelTextView = findViewById(R.id.accelTextView);
+        accelSeekBar = findViewById(R.id.accelSeekBar);
+
+        steerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                steerTextView.setText(progress - 50 + "°");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        accelSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                accelTextView.setText(progress / 10.0 + " mph");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
