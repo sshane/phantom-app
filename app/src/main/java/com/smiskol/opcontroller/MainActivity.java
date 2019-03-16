0d6c37a8c907bdec513d211c558077f9d9427097
Shane <shane@smiskol.com>
2019-03-16 15:48:37 -0500

    Added some logic from speed controller

--- app/src/main/java/com/smiskol/opcontroller/MainActivity.java
+++ app/src/main/java/com/smiskol/opcontroller/MainActivity.java
@@ -1,11 +1,28 @@
 package com.smiskol.opcontroller;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences preferences;
     CustomSeekBar steerSeekBar;
     TextView steerTextView;
     TextView accelTextView;
@@ -20,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
         steerTextView = findViewById(R.id.steerTextView);
         accelTextView = findViewById(R.id.accelTextView);
         accelSeekBar = findViewById(R.id.accelSeekBar);
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        doWelcome();
 
         steerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -55,4 +75,165 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void doWelcome(){
+        if (preferences.getBoolean("welcome", true)){
+            welcomeDialog();
+        }
+    }
+
+    public void requestPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (preferences.getBoolean("welcome", true)) {
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        writeSupportingFiles();
+                        preferences.edit().putBoolean("welcome", false).apply();
+                        new AlertDialog.Builder(this).setTitle("Granted!")
+                                .setMessage("Awesome! Permission granted. I've written the EON private key to a file for us to read when we make connections to the EON later.")
+                                .setPositiveButton("Cool", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                    }
+                                }).show();
+                    } else {
+                        new AlertDialog.Builder(this).setTitle("Uh oh!")
+                                .setMessage("You've denied the storage permission. We need this to write the EON private key to a file so we can make connections over SSH. Please accept the permission.")
+                                .setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                    }
+                                })
+                                .setOnDismissListener(new DialogInterface.OnDismissListener() {
+                                    @Override
+                                    public void onDismiss(DialogInterface dialog) {
+                                        requestPermission();
+                                    }
+                                })
+                                .show();
+                    }
+                }
+                return;
+            }
+        }
+    }
+
+    public void welcomeDialog() {
+        new AlertDialog.Builder(this).setTitle("Welcome!")
+                .setMessage("op Controller is an app that can remotely control your car's acceleration and turning angle via SSH. We will now request the data permission, required to access your EON.")
+                .setPositiveButton("Got it", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).setCancelable(false)
+                .setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermission();
+                    }
+                })
+                .show();
+    }
+
+    public void writeSupportingFiles() {
+        try {
+            File file = new File(getFilesDir(), "eon_id.ppk");
+            if (!file.exists() || file.length() == 0) {
+                FileOutputStream fOut = new FileOutputStream(file);
+                OutputStreamWriter osw = new OutputStreamWriter(fOut);
+
+                String privateKey = "-----BEGIN RSA PRIVATE KEY-----\n" +
+                        "MIIEogIBAAKCAQEAvol16t9E6vieTSmrdylhws3JsGeeZxoeloIAKhAmuQmrAZTP\n" +
+                        "VXkTqVbt23gPuYdDIm0YGw+AzLVVwbeoBL2fJ3dOBO3iwPS02chQ2e0pEjlY+KFz\n" +
+                        "kLE9BpyZiqwEluSrJU1qlc036NlwrWftNOIpC8ZshXgTvDTnBK1taWvIBXUA06B/\n" +
+                        "RawO5IMrInP11REkzqHu15c0aHv3mWnBEPo7Z5hXdtQOGhAA5JNNAIY69LimiYi1\n" +
+                        "AD2rcbNonCF1qYGLX6qrWihdt8EretTk7unAMF2zlq95viFEkVDtCEcxCEEt89Y3\n" +
+                        "3dbL4M0oEksGdS4Y+AKCsSBACHPKiazuLspgiQIDAQABAoIBAQCEhXr8RxnaC92e\n" +
+                        "cZMOqDuUkCjthsRHlYUczYJrvxwPqsfDq8qg+jtQlmONN+5H7eolsZcIizncJ2tj\n" +
+                        "9ubnlTNy8anUB9ikuA5pQsfpKuhcAoL9Ot30DzIQvS6Vopr2kEjxAu1VD40JaOLT\n" +
+                        "2OrE02AVDodANYoUZv8e47irkAlosQqvAvw1ZwdV+Jho/lt5yXOU8FSbYCW24ga6\n" +
+                        "uj1q4bwf96ppMR0S+3VNkgW9ojURdSy2N9HScf3A+91AyjR65a7I5N1CXNvTKePz\n" +
+                        "JWnSr1JEajcJWMUrgLSVdJ2d/ohZC7N2nUkx3SaQpUHq+OUedaxQ5VbA89mQaW/4\n" +
+                        "UTUaBg7hAoGBAOgNRIsS6u0GDod3G14cod1uJKVbwPxT3yh9TjMtzjTg/2PTmvjP\n" +
+                        "8LYVtcEqES9p/rriFuTgIUyLyBIr4+mwGbE097cK7zq72Lva8fWpZ+KfAYcr3Y3l\n" +
+                        "uJEu0/BT+aJei6DrdrEz909SzriTzrkLzo5SjyiDId3N0RTVk5xszD2tAoGBANIz\n" +
+                        "Yjy8T9wNp619JHigyPlR7rzmPPIHYdFuQbizC6LziA5PWkBSGwWzLltTk4xyr/TS\n" +
+                        "vi68PmGwhajhn9XVP1DeYEshPJV/0BbFBlKlGcee+JyWZziHMtzjTp0C3LxwEE6C\n" +
+                        "xQBlHez1oD9wrR5LfYRL9pKFMC+L6IpEz9bvRpHNAoGBANmqaFsT2a2Pet1yygcT\n" +
+                        "UHnGMTWyxWlquu7d6xZypvRPAQCAouM1GhOSdbTFYu1YvYpLPTJfUpzcmUUCSn0P\n" +
+                        "pGnmx125MgGj5n7/tuq6hym6ANLsQJwzmVcF1+OcwZKeoNbHR8ScfCS6BhJ5AvXs\n" +
+                        "r0otAv/7US8fOjoSxK18GHDZAn9YrVTESq1mKFyU1DaOrUYb6HTPPFJ5yKN7twgC\n" +
+                        "44YFOLgtUUzB1eGQhgcIgDm/BqM0pbOWA9RNYisBFC5aB5yugSIej+b/Kuyern/8\n" +
+                        "XaqCjI5VgR4Kuv66MSr5EjwNQzmd5Y02nXIChZ0VJnPiU/af2WwsZAPwCxYPPvhv\n" +
+                        "tIIRAoGAPLxtzP7rcHi76uESO5e1O2/otgWo3ytjpszYv8boH3i42OpNrX0Bkbr+\n" +
+                        "qaU43obY4trr4A1pIIyVID32aYq9yEbFTFIhYJaFhhxEzstEL3OQMLakyRS0w9Vs\n" +
+                        "2trgYpUlSBLIOmPNxonJIfnozphLGOnKNe0RWgGR8BnwhRYzu+k=\n" +
+                        "-----END RSA PRIVATE KEY-----\n";
+
+                osw.write(privateKey);
+
+                osw.close();
+                System.out.println("File written");
+            } else {
+                System.out.println("File already written");
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            makeSnackbar("Error writing support files.");
+        }
+
+        try {
+            File file = new File(getFilesDir(), "controller_listener.py");
+            if (!file.exists() || file.length() == 0) {
+                FileOutputStream fOut = new FileOutputStream(file);
+                OutputStreamWriter osw = new OutputStreamWriter(fOut);
+
+                String controllerListener = "import sys\n" +
+                        "live_controller_file = '/data/live_controller_file'\n" +
+                        "\n" +
+                        "def write_file(a):\n" +
+                        "    try:\n" +
+                        "        with open(live_controller_file, 'r') as speed:\n" +
+                        "            modified_speed=float(speed.read())+a\n" +
+                        "        with open(live_controller_file, 'w') as speed:\n" +
+                        "            speed.write(str(modified_speed))\n" +
+                        "    except: #in case file doesn't exist or is empty\n" +
+                        "        with open(live_controller_file, 'w') as speed:\n" +
+                        "            speed.write(str(28.0))\n" +
+                        "\n" +
+                        "if __name__ == \"__main__\":\n" +
+                        "    write_file(int(sys.argv[1]))";
+
+                osw.write(controllerListener);
+
+                osw.close();
+                System.out.println("File written");
+            } else {
+                System.out.println("File already written");
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            makeSnackbar("Error writing support files.");
+        }
+
+    }
+
+    public void makeSnackbar(String s) {
+        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), s, Snackbar.LENGTH_SHORT);
+
+        snackbar.show();
+    }
+
 }
