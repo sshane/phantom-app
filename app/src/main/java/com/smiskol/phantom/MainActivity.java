4ccb8ae392e010dfab10139723f2f3329d5a0d29
Shane <shane@smiskol.com>
2019-05-01 04:42:01 -0500

    data permission no longer required

--- app/src/main/java/com/smiskol/phantom/MainActivity.java
+++ app/src/main/java/com/smiskol/phantom/MainActivity.java
@@ -1,18 +1,15 @@
 package com.smiskol.phantom;
 
-import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -25,12 +22,9 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -306,29 +300,6 @@ public class MainActivity extends AppCompatActivity implements WelcomeFragment.O
         }
     }
 
-    public void requestPermission() {
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                1);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 1: {
-                if (preferences.getBoolean("welcome", true)) {
-                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        writeSupportingFiles();
-                        preferences.edit().putBoolean("welcome", false).apply();
-                        successDialog();
-                    } else {
-                        uhohDialog();
-                    }
-                }
-            }
-        }
-    }
-
     public void outOfDate() {
         AlertDialog successDialog = new AlertDialog.Builder(this).setTitle("Out of date!")
                 .setMessage("You're on an old version of Phantom. Updating is highly recommended.")
@@ -341,24 +312,6 @@ public class MainActivity extends AppCompatActivity implements WelcomeFragment.O
 
                 }).show();
 
-        int titleText = getResources().getIdentifier("alertTitle", "id", "android");
-        ((TextView) successDialog.getWindow().findViewById(titleText)).setTypeface(semibold);
-        TextView tmpMessage = successDialog.getWindow().findViewById(android.R.id.message);
-        Button tmpButton = successDialog.getWindow().findViewById(android.R.id.button1);
-        tmpMessage.setTypeface(regular);
-        tmpButton.setTypeface(semibold);
-    }
-
-    public void successDialog() {
-        AlertDialog successDialog = new AlertDialog.Builder(this).setTitle("Granted!")
-                .setMessage("You're all set now, Captain.")
-                .setPositiveButton("Rad", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-
-                }).show();
         int titleText = getResources().getIdentifier("alertTitle", "id", "android");
         ((TextView) successDialog.getWindow().findViewById(titleText)).setTypeface(semibold);
         TextView tmpMessage = successDialog.getWindow().findViewById(android.R.id.message);
@@ -387,18 +340,13 @@ public class MainActivity extends AppCompatActivity implements WelcomeFragment.O
         tmpButton.setTypeface(semibold);
     }
 
-    public void uhohDialog() {
-        AlertDialog successDialog = new AlertDialog.Builder(this).setTitle("Uh oh!")
-                .setMessage("You've denied the storage permission. We need this to write the EON private key to a file so we can make connections over SSH.")
-                .setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+    public void welcomeDialog() {
+        AlertDialog successDialog = new AlertDialog.Builder(this).setTitle("Welcome!")
+                .setMessage("Phantom is an experimental app that can remotely control your car's acceleration and wheel angle via SSH.")
+                .setPositiveButton("Sick", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                }).setOnDismissListener(new DialogInterface.OnDismissListener() {
-                    @Override
-                    public void onDismiss(DialogInterface dialog) {
-                        requestPermission();
+                        infoDialog();
                     }
                 }).setCancelable(false)
                 .show();
@@ -411,18 +359,13 @@ public class MainActivity extends AppCompatActivity implements WelcomeFragment.O
         tmpButton.setTypeface(semibold);
     }
 
-    public void welcomeDialog() {
-        AlertDialog successDialog = new AlertDialog.Builder(this).setTitle("Welcome!")
-                .setMessage("Phantom is an experimental app that can remotely control your car's acceleration and wheel angle via SSH. We will now request the data permission, required to access your EON.")
-                .setPositiveButton("Sick", new DialogInterface.OnClickListener() {
+    public void infoDialog() {
+        AlertDialog successDialog = new AlertDialog.Builder(this).setTitle("Reboot recommended")
+                .setMessage("After you first successfully connect with Phantom, a reboot of your EON is recommended. We append an ssh setting that greatly reduces the latency of sending Phantom commands.")
+                .setPositiveButton("Got it", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                }).setOnDismissListener(new DialogInterface.OnDismissListener() {
-                    @Override
-                    public void onDismiss(DialogInterface dialog) {
-                        requestPermission();
+                        preferences.edit().putBoolean("welcome", false).apply();
                     }
                 }).setCancelable(false)
                 .show();
@@ -436,7 +379,7 @@ public class MainActivity extends AppCompatActivity implements WelcomeFragment.O
     }
 
     public Boolean openSession(String eonIP) {
-        eonConnection = sshClass.getConnection(MainActivity.this, eonIP);
+        eonConnection = sshClass.getConnection(eonIP);
         if (eonConnection == null) {
             System.out.println("failed to open connection");
             return false;
@@ -509,60 +452,10 @@ public class MainActivity extends AppCompatActivity implements WelcomeFragment.O
         }
     }
 
-
     public void doDisable() {
         viewPager.setCurrentItem(0);
         adapter.setViewCount(1);
         tabLayout.setVisibility(View.GONE);
-    }
-
-    public void writeSupportingFiles() {
-        try {
-            File file = new File(getFilesDir(), "eon_id.ppk");
-            if (!file.exists() || file.length() == 0) {
-                FileOutputStream fOut = new FileOutputStream(file);
-                OutputStreamWriter osw = new OutputStreamWriter(fOut);
-
-                String privateKey = "-----BEGIN RSA PRIVATE KEY-----\n" +
-                        "MIIEogIBAAKCAQEAvol16t9E6vieTSmrdylhws3JsGeeZxoeloIAKhAmuQmrAZTP\n" +
-                        "VXkTqVbt23gPuYdDIm0YGw+AzLVVwbeoBL2fJ3dOBO3iwPS02chQ2e0pEjlY+KFz\n" +
-                        "kLE9BpyZiqwEluSrJU1qlc036NlwrWftNOIpC8ZshXgTvDTnBK1taWvIBXUA06B/\n" +
-                        "RawO5IMrInP11REkzqHu15c0aHv3mWnBEPo7Z5hXdtQOGhAA5JNNAIY69LimiYi1\n" +
-                        "AD2rcbNonCF1qYGLX6qrWihdt8EretTk7unAMF2zlq95viFEkVDtCEcxCEEt89Y3\n" +
-                        "3dbL4M0oEksGdS4Y+AKCsSBACHPKiazuLspgiQIDAQABAoIBAQCEhXr8RxnaC92e\n" +
-                        "cZMOqDuUkCjthsRHlYUczYJrvxwPqsfDq8qg+jtQlmONN+5H7eolsZcIizncJ2tj\n" +
-                        "9ubnlTNy8anUB9ikuA5pQsfpKuhcAoL9Ot30DzIQvS6Vopr2kEjxAu1VD40JaOLT\n" +
-                        "2OrE02AVDodANYoUZv8e47irkAlosQqvAvw1ZwdV+Jho/lt5yXOU8FSbYCW24ga6\n" +
-                        "uj1q4bwf96ppMR0S+3VNkgW9ojURdSy2N9HScf3A+91AyjR65a7I5N1CXNvTKePz\n" +
-                        "JWnSr1JEajcJWMUrgLSVdJ2d/ohZC7N2nUkx3SaQpUHq+OUedaxQ5VbA89mQaW/4\n" +
-                        "UTUaBg7hAoGBAOgNRIsS6u0GDod3G14cod1uJKVbwPxT3yh9TjMtzjTg/2PTmvjP\n" +
-                        "8LYVtcEqES9p/rriFuTgIUyLyBIr4+mwGbE097cK7zq72Lva8fWpZ+KfAYcr3Y3l\n" +
-                        "uJEu0/BT+aJei6DrdrEz909SzriTzrkLzo5SjyiDId3N0RTVk5xszD2tAoGBANIz\n" +
-                        "Yjy8T9wNp619JHigyPlR7rzmPPIHYdFuQbizC6LziA5PWkBSGwWzLltTk4xyr/TS\n" +
-                        "vi68PmGwhajhn9XVP1DeYEshPJV/0BbFBlKlGcee+JyWZziHMtzjTp0C3LxwEE6C\n" +
-                        "xQBlHez1oD9wrR5LfYRL9pKFMC+L6IpEz9bvRpHNAoGBANmqaFsT2a2Pet1yygcT\n" +
-                        "UHnGMTWyxWlquu7d6xZypvRPAQCAouM1GhOSdbTFYu1YvYpLPTJfUpzcmUUCSn0P\n" +
-                        "pGnmx125MgGj5n7/tuq6hym6ANLsQJwzmVcF1+OcwZKeoNbHR8ScfCS6BhJ5AvXs\n" +
-                        "r0otAv/7US8fOjoSxK18GHDZAn9YrVTESq1mKFyU1DaOrUYb6HTPPFJ5yKN7twgC\n" +
-                        "44YFOLgtUUzB1eGQhgcIgDm/BqM0pbOWA9RNYisBFC5aB5yugSIej+b/Kuyern/8\n" +
-                        "XaqCjI5VgR4Kuv66MSr5EjwNQzmd5Y02nXIChZ0VJnPiU/af2WwsZAPwCxYPPvhv\n" +
-                        "tIIRAoGAPLxtzP7rcHi76uESO5e1O2/otgWo3ytjpszYv8boH3i42OpNrX0Bkbr+\n" +
-                        "qaU43obY4trr4A1pIIyVID32aYq9yEbFTFIhYJaFhhxEzstEL3OQMLakyRS0w9Vs\n" +
-                        "2trgYpUlSBLIOmPNxonJIfnozphLGOnKNe0RWgGR8BnwhRYzu+k=\n" +
-                        "-----END RSA PRIVATE KEY-----\n";
-
-                osw.write(privateKey);
-
-                osw.close();
-                System.out.println("File written");
-            } else {
-                System.out.println("File already written");
-            }
-
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-            makeSnackbar("Error writing support files.");
-        }
     }
 
     public void makeSnackbar(String s) {
@@ -571,5 +464,4 @@ public class MainActivity extends AppCompatActivity implements WelcomeFragment.O
         tv.setTypeface(regular);
         snackbar.show();
     }
-
 }
--- app/src/main/java/com/smiskol/phantom/SSHClass.java
+++ app/src/main/java/com/smiskol/phantom/SSHClass.java
@@ -9,13 +9,41 @@ import ch.ethz.ssh2.Connection;
 import ch.ethz.ssh2.Session;
 
 public class SSHClass {
-    public Connection getConnection(Context context, String eonIP) {
+    String privateKey = "-----BEGIN RSA PRIVATE KEY-----\n" +
+            "MIIEogIBAAKCAQEAvol16t9E6vieTSmrdylhws3JsGeeZxoeloIAKhAmuQmrAZTP\n" +
+            "VXkTqVbt23gPuYdDIm0YGw+AzLVVwbeoBL2fJ3dOBO3iwPS02chQ2e0pEjlY+KFz\n" +
+            "kLE9BpyZiqwEluSrJU1qlc036NlwrWftNOIpC8ZshXgTvDTnBK1taWvIBXUA06B/\n" +
+            "RawO5IMrInP11REkzqHu15c0aHv3mWnBEPo7Z5hXdtQOGhAA5JNNAIY69LimiYi1\n" +
+            "AD2rcbNonCF1qYGLX6qrWihdt8EretTk7unAMF2zlq95viFEkVDtCEcxCEEt89Y3\n" +
+            "3dbL4M0oEksGdS4Y+AKCsSBACHPKiazuLspgiQIDAQABAoIBAQCEhXr8RxnaC92e\n" +
+            "cZMOqDuUkCjthsRHlYUczYJrvxwPqsfDq8qg+jtQlmONN+5H7eolsZcIizncJ2tj\n" +
+            "9ubnlTNy8anUB9ikuA5pQsfpKuhcAoL9Ot30DzIQvS6Vopr2kEjxAu1VD40JaOLT\n" +
+            "2OrE02AVDodANYoUZv8e47irkAlosQqvAvw1ZwdV+Jho/lt5yXOU8FSbYCW24ga6\n" +
+            "uj1q4bwf96ppMR0S+3VNkgW9ojURdSy2N9HScf3A+91AyjR65a7I5N1CXNvTKePz\n" +
+            "JWnSr1JEajcJWMUrgLSVdJ2d/ohZC7N2nUkx3SaQpUHq+OUedaxQ5VbA89mQaW/4\n" +
+            "UTUaBg7hAoGBAOgNRIsS6u0GDod3G14cod1uJKVbwPxT3yh9TjMtzjTg/2PTmvjP\n" +
+            "8LYVtcEqES9p/rriFuTgIUyLyBIr4+mwGbE097cK7zq72Lva8fWpZ+KfAYcr3Y3l\n" +
+            "uJEu0/BT+aJei6DrdrEz909SzriTzrkLzo5SjyiDId3N0RTVk5xszD2tAoGBANIz\n" +
+            "Yjy8T9wNp619JHigyPlR7rzmPPIHYdFuQbizC6LziA5PWkBSGwWzLltTk4xyr/TS\n" +
+            "vi68PmGwhajhn9XVP1DeYEshPJV/0BbFBlKlGcee+JyWZziHMtzjTp0C3LxwEE6C\n" +
+            "xQBlHez1oD9wrR5LfYRL9pKFMC+L6IpEz9bvRpHNAoGBANmqaFsT2a2Pet1yygcT\n" +
+            "UHnGMTWyxWlquu7d6xZypvRPAQCAouM1GhOSdbTFYu1YvYpLPTJfUpzcmUUCSn0P\n" +
+            "pGnmx125MgGj5n7/tuq6hym6ANLsQJwzmVcF1+OcwZKeoNbHR8ScfCS6BhJ5AvXs\n" +
+            "r0otAv/7US8fOjoSxK18GHDZAn9YrVTESq1mKFyU1DaOrUYb6HTPPFJ5yKN7twgC\n" +
+            "44YFOLgtUUzB1eGQhgcIgDm/BqM0pbOWA9RNYisBFC5aB5yugSIej+b/Kuyern/8\n" +
+            "XaqCjI5VgR4Kuv66MSr5EjwNQzmd5Y02nXIChZ0VJnPiU/af2WwsZAPwCxYPPvhv\n" +
+            "tIIRAoGAPLxtzP7rcHi76uESO5e1O2/otgWo3ytjpszYv8boH3i42OpNrX0Bkbr+\n" +
+            "qaU43obY4trr4A1pIIyVID32aYq9yEbFTFIhYJaFhhxEzstEL3OQMLakyRS0w9Vs\n" +
+            "2trgYpUlSBLIOmPNxonJIfnozphLGOnKNe0RWgGR8BnwhRYzu+k=\n" +
+            "-----END RSA PRIVATE KEY-----\n";
+
+    public Connection getConnection(String eonIP) {
         String username = "root";
-        File keyfile = new File(context.getFilesDir(), "eon_id.ppk");
         try {
             Connection connection = new Connection(eonIP, 8022);
             connection.connect();
-            boolean isAuthenticated = connection.authenticateWithPublicKey(username, keyfile, "");
+            Boolean isAuthenticated = connection.authenticateWithPublicKey(username, privateKey.toCharArray(), "");
+            //boolean isAuthenticated = connection.authenticateWithPublicKey(username, keyfile, "");
             if (!isAuthenticated) {
                 return null;
             }
@@ -25,6 +53,7 @@ public class SSHClass {
             return null;
         }
     }
+
     public Boolean sendPhantomCommand(Connection conn, String enabled, String desiredSpeed, String steeringAngle, String time) {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
